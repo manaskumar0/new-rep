999dc6d3059ad93d147935c41e1ac102b4e93349
lcom145-one <manas7337@gmail.com>
2018-12-14 18:02:47 +0530

    test

--- tests/server.test.js
+++ tests/server.test.js
@@ -4,7 +4,14 @@ const request = require('supertest');
 const {app} = require('./../server/server');
 const {todo} = require('./../server/models/todo');
 
+const Todo = [{
+    text: 'First Test Todo'
+}, {
+    text: 'Second Test Todo'
+}];
+
 beforeEach((done) => {
+    todo.insertMany(Todo)
    todo.remove({}).then(() => {
        done();
 })
@@ -25,7 +32,7 @@ describe('Post TODOS', () => {
            if(err)
                return done(err);
 
-           todo.find().then((todo) =>{
+           todo.find({text}).then((todo) =>{
                expect(todo.length).toBe(1);
                expect(todo[0].text).toBe(text);
                done();
@@ -44,11 +51,24 @@ describe('Post TODOS', () => {
             if(err)
                 return done(err);
             todo.find().then((todo) =>{
-                expect(todo.length).toBe(0);
+                expect(todo.length).toBe(2);
                 done();
             }).catch ((err) => {
                 done(err);
 });
    })
    })
+});
+
+
+describe('Get', () => {
+   it('should get all todos', (done) => {
+    request(app)
+        .get('/todo')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.length).toBe(2);
+    })
+    .end(done);
+});
 });
